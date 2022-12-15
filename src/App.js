8e1dc46b692c49a060605194891534c223c78dd1
shreyasi23 <vasistashreyasi@gmail.com>
2022-12-15 22:09:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 //import logo from './logo.svg';
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg app-color">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/#">TextUtils</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/#">About</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar title = "TextUtils2" aboutText = "About us"/>
+      {/* <Navbar/> */}
+      {/* <Navbar aboutText = "About us"/> */}
     </>
   );
 }
