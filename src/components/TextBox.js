238f2cf975f1d61d9e02e1c186b155d0694360d6
shreyasi23 <vasistashreyasi@gmail.com>
2022-12-21 22:04:27 +0530

    titlecase clear find and replace

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 
 export default function TextBox(props){
     const[text, setText] = useState("Enter text here");
+    const[fText, set_fText] = useState("Enter word to find");
+    const[rText, set_rText] = useState("Enter word to replace");
     
     // function to handle click event for uppercase button
     const handleUppercase = (event) =>{
@@ -22,16 +24,61 @@ export default function TextBox(props){
         let updateText = text.toLowerCase();
         setText(updateText);
     }
+
+    // function to handle click event for clear
+    const handleClear = (event) =>{
+        console.log("clicked on clear button");
+        let updateText = '';
+        setText(updateText);
+    }
+
+    // function to handle click event for title case 
+    const handleTitlecase = (event) =>{
+        console.log("clicked on title case button");
+        let updateText = text.toLowerCase().split(' ');
+        for(let i = 0; i < updateText.length; i++){
+            updateText[i] = updateText[i].charAt(0).toUpperCase() + updateText[i].slice(1);
+        }
+        setText(updateText.join(' '))
+    }
+
+    // function to handle find 
+    const handleFind = (event) =>{
+        console.log("handling on change event to change the state of the find box");
+        set_fText(event.target.value);
+    }
+
+    // function to handle replace
+    const handleReplace = (event) =>{
+        console.log("handling on change event to change the state of the replace box");
+        set_rText(event.target.value);
+    }
+
+    // function to handle click event of replace button
+    const handleClickReplace = () =>{
+        let updateText = text.replace(fText, rText);
+        setText(updateText);
+    }
+
     return(
         <>
             {/*text editing*/}
             <section id = "text_edit">
                 <h2 >{props.heading}</h2>
-                <p >Easily convert your text to uppercase, lowercase</p>
+                <p >Easily convert your text to uppercase, lowercase, titlecase, get count of words and characters and get read time</p>
                 <textarea className="form-control input_box" id="exampleFormControlTextarea1" value = {text} onChange = {handleOnChange} rows="8"></textarea>
                 <div id = "button_box">
-                    <button className = "btn btn-info my_btn_style"  onClick = {handleUppercase}>[A] uppercase</button>
-                    <button className = "btn btn-info my_btn_style" onClick = {handleLowercase}>[a] lowercase</button>
+                    <button className = "btn btn-info my_btn_style"  onClick = {handleUppercase}>[A] Uppercase</button>
+                    <button className = "btn btn-info my_btn_style" onClick = {handleLowercase}>[a] Lowercase</button>
+                    <button className = "btn btn-info my_btn_style resize_btn" onClick = {handleTitlecase}>Titlecase</button>
+                    <button className = "btn btn-info my_btn_style resize_btn" id  = "clear_btn" onClick = {handleClear}><i class="fa fa-eraser"></i> Clear</button>
+                </div>
+                <div id = "find_replace_area">
+                    <span>Find</span>
+                    <input type = "text" value = {fText} onChange = {handleFind}/>
+                    <span>Replace</span>
+                    <input type = "text" value = {rText} onChange = {handleReplace}/>
+                    <button className = "btn btn-info my_btn_style resize_btn" onClick = {handleClickReplace}>Replace</button>
                 </div>
             </section>
 
@@ -47,7 +94,7 @@ export default function TextBox(props){
             <section id = "text_preview">
                 <h4 id = "tp_heading">Preview</h4>
                 {/* <div id = "preview_display">{text}</div> */}
-                <textarea className="form-control preview_box" id="exampleFormControlTextarea1" rows = "20" cols= "200" value={text}></textarea>
+                <textarea className="form-control preview_box" id="exampleFormControlTextarea1" rows = "19" cols= "200" value={text}></textarea>
             </section>
         </> 
     )
