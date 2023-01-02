db74fd6978eb0d3e62caff4e221a15688876d565
shreyasi23 <vasistashreyasi@gmail.com>
2023-01-02 14:03:46 +0530

    modified about

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ export default function About(props){
     return(
         <section>
             <h2 style = {{color: props.mode === 'light'?'black':'white'}}>About TextUtils</h2>
-            <div style = {{color: props.mode === 'light'?'black':'white'}}>TextUtils is a free tool to help you with your daily tasks. Convert your entire text to uppercase, lowercase, titlecase, find and replace words, remove extra spaces in text and copy text with just one click. TextUtils also provides text analysis which inclues word and character count and read time.</div>
+            <div style = {{color: props.mode === 'light'?'black':'white'}}>TextUtils is a free tool to help you with your daily tasks. Convert your entire text to uppercase, lowercase, titlecase, find and replace words, remove extra spaces in text and copy text with just one click. TextUtils also provides text analysis which includes word and character count and read time.</div>
         </section>
     )
 }
